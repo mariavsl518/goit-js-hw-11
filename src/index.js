17622966a4707518f171fa83a180e1293ee1fabd
mariavsl518 <mariavsl518@gmail.com>
2023-11-18 20:34:23 +0100

    raw

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import axios from "axios";
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
 const form = document.querySelector('.search-form');
-form.addEventListener('submit', handleSubmit)
+const gallery = document.querySelector('.gallery');
+const load = document.querySelector('.load-more');
+form.addEventListener('submit', handleSubmit);
+load.addEventListener('click', handleClick);
+
 const baseURL = 'https://pixabay.com/api/';
 
 const params = new URLSearchParams({
@@ -9,40 +16,73 @@ const params = new URLSearchParams({
     q: '',
     image_type: 'photo',
     orientation: 'horizontal',
-    safesearch: 'true'
+    safesearch: true,
+    per_page: 40,
 })
+let page = 1;
+load.hidden = true;
+const galleryBox = new SimpleLightbox('.gallery .photo-card a');
 
 async function getImages(params) {
-    // if (params.q === '') {
-    //     throw new Error('')
-    //  }
-    return axios.get(`${baseURL}?${params}`)
-        .then((resp) => {return resp.data.hits })
-        // .catch(() => { })
+    if (!form.elements.searchQuery.value) {
+        gallery.innerHTML = '';
+        Notify.failure('Sorry, there are no images matching your search query. Please try again.')
+    }
+    else {
+        return await axios.get(`${baseURL}?${params}&page=${page}`)
+            .then((resp) => {
+                load.hidden = false;
+                return resp.data.hits;
+            })
+    }
 }
 
-function handleSubmit(evt) { 
+async function getNewImages(params) {
+    page +=1;
+    return await axios.get(`${baseURL}?${params}&page=${page}`)
+        .then((resp) => { return resp.data.hits });
+}
+
+function handleSubmit(evt) {
     evt.preventDefault();
+    page = 1;
+
     const request = form.elements.searchQuery.value;
     params.set('q', request);
-    console.log(getImages(params));
+    getImages(params)
+        .then(resp => gallery.innerHTML = createMarkup(resp))
+        .catch(new Error("Sorry, there are no images matching your search query. Please try again."));
+    form.reset();
 }
 
-// `<div class="photo-card">
-//   <img src="" alt="" loading="lazy" />
-//   <div class="info">
-//     <p class="info-item">
-//       <b>Likes</b>
-//     </p>
-//     <p class="info-item">
-//       <b>Views</b>
-//     </p>
-//     <p class="info-item">
-//       <b>Comments</b>
-//     </p>
-//     <p class="info-item">
-//       <b>Downloads</b>
-//     </p>
-//   </div>
-// </div>`
+function handleClick() { 
+    getNewImages(params)
+        .then(resp => gallery.insertAdjacentHTML('beforeend', createMarkup(resp)))
+        .catch(new Error("Sorry, there are no images matching your search query. Please try again."));
+    galleryBox.refresh();
+}
 
+function createMarkup(obj) {
+
+     return obj.map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) =>
+        `
+        <div class="photo-card">
+        <a href="${largeImageURL}">
+        <img src="${webformatURL}" alt="${tags}" loading="lazy" width="500px"/>
+        </a>
+        <div class="info">
+            <p class="info-item">
+            <b>Likes:${likes}</b>
+            </p>
+            <p class="info-item">
+            <b>Views:${views}</b>
+            </p>
+            <p class="info-item">
+            <b>Comments:${comments}</b>
+            </p>
+            <p class="info-item">
+            <b>Downloads:${downloads}</b>
+            </p>
+        </div>
+        </div>`).join('');
+};
