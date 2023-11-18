e0ba1c4f01c8e7fabbea9e99611c0f612b2c4eee
mariavsl518 <mariavsl518@gmail.com>
2023-11-18 20:54:00 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -19,6 +19,7 @@ const params = new URLSearchParams({
     safesearch: true,
     per_page: 40,
 })
+
 let page = 1;
 load.hidden = true;
 const galleryBox = new SimpleLightbox('.gallery .photo-card a');
@@ -26,7 +27,7 @@ const galleryBox = new SimpleLightbox('.gallery .photo-card a');
 async function getImages(params) {
     if (!form.elements.searchQuery.value) {
         gallery.innerHTML = '';
-        Notify.failure('Sorry, there are no images matching your search query. Please try again.')
+        throw Notify.failure('Sorry, there are no images matching your search query. Please try again.')
     }
     else {
         return await axios.get(`${baseURL}?${params}&page=${page}`)
@@ -52,21 +53,22 @@ function handleSubmit(evt) {
     getImages(params)
         .then(resp => gallery.innerHTML = createMarkup(resp))
         .catch(new Error("Sorry, there are no images matching your search query. Please try again."));
+    
     form.reset();
 }
 
 function handleClick() { 
     load.hidden = true;
+    
     getNewImages(params)
         .then(resp => {
             load.hidden = false
             return gallery.insertAdjacentHTML('beforeend', createMarkup(resp));
         })
         .catch(new Error("Sorry, there are no images matching your search query. Please try again."));
+    
     galleryBox.refresh();
 }
-
-// function 
 
 function createMarkup(obj) {
 
